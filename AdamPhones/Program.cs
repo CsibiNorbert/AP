b101a1056ae36b359294c1e16ae5b52ccaa58d36
Norbert Csibi <cibi_norbert@yahoo.com>
2020-01-26 04:29:17 -0800

    Add project files.

---
+++ AdamPhones/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdamPhones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
